1f77ac38e1b206c140a6befbd1a169fd5cfbea2f
yungyung5002 <yungyung5002@gmail.com>
2026-09-11 15:17:25 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'msw-timetable-v1';
+const ASSETS = [
+  './',
+  './msw-timetable.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((res) => {
+          const cp = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, cp));
+          return res;
+        })
+        .catch(() => caches.match('./msw-timetable.html'));
+    })
+  );
+});
